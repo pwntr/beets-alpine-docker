043fd9a454790d7293ce77012af0ffb2d26a1b85
Peter Winter <pwntr@users.noreply.github.com>
2016-02-23 19:09:56 +0100

    Update testflacs.sh

--- testflacs.sh
+++ testflacs.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-# tests [-t] if flac files in import dir are corrupted, treat warnings as errors [-w], keep quiet otherwise [-s]
+# tests [-t] if flac files in import dir are corrupted, treat warnings as errors [-w], keep quiet otherwise [-s].
+# IFS=$'\n' fixes an issue with spaces in file/folder names. 
 
-for file in `find /import -type f -name *.flac`; do flac -tws $file; done
+IFS=$'\n'
+for file in $(find /import/ -type f -iname '*.flac'); do
+    flac -tws $file;
+done
